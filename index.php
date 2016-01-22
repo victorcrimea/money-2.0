598662bfa39fb0a59e9e0999e591a31660a0ca91
victorcrimea <victor_crimea@mail.ru>
2016-01-22 05:05:05 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head> 
+	<title>Копилка 1.1</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+	<script type='text/javascript'>
+ $(document).ready(function(){
+	alert();
+});
+</script>
+</head>
+
+<body>
+	<div id='side-panel'></div>
+	<div id=''></div>
+</body>
+</html";
+
